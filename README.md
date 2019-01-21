946ba46e85b64812a8b50d4139a96448c006d948
David Cruz Anaya <cruzanayadavid@gmail.com>
2019-01-21 19:00:42 +0000

    Part 2 and edit README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # KeyframeAnimations
 Example of Android keyframe animations with ConstraintLayouts
+
+###Part 1
+- Simple Lineat Constraint Amnimation
+
+###Part 2
+- Customised animation and interpolations
+
+2019 Created by David Cruz Anaya
--- app/src/main/java/com/example/keyframeanimations/MainActivity.java
+++ app/src/main/java/com/example/keyframeanimations/MainActivity.java
@@ -5,17 +5,28 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.ChangeTransform;
 import android.transition.TransitionManager;
+import android.transition.TransitionValues;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
     private ConstraintSet constraintSetStart;
+    private ConstraintSet constraintSetHalf;
     private ConstraintSet constraintSetEnd;
 
     private ConstraintLayout mainContainer;
 
-    private Boolean isToggled = false;
+    // Animation Transitions
+    private ChangeBounds changeBounds;
+
+    private int animationNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +35,19 @@ public class MainActivity extends AppCompatActivity {
 
         mainContainer = findViewById(R.id.main_container);
 
+        changeBounds = new ChangeBounds();
+
+        changeBounds.setInterpolator(
+//                new AnticipateInterpolator(10.0f)
+//                new DecelerateInterpolator(10.0f)
+                new AnticipateOvershootInterpolator(1.0f)
+        );
+
         constraintSetStart = new ConstraintSet();
         constraintSetStart.clone(this, R.layout.activity_main_animation_start);
+
+        constraintSetHalf = new ConstraintSet();
+        constraintSetHalf.clone(this, R.layout.activity_main_animation_half);
 
         constraintSetEnd = new ConstraintSet();
         constraintSetEnd.clone(this, R.layout.activity_main_animation_end);
@@ -41,14 +63,26 @@ public class MainActivity extends AppCompatActivity {
         mainContainer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TransitionManager.beginDelayedTransition(mainContainer);
+                // Standard animation (Linear)
+//                TransitionManager.beginDelayedTransition(mainContainer);
 
-                if(!isToggled) {
+                // Custom Animation
+//                TransitionManager.beginDelayedTransition(mainContainer, changeBounds);
+
+                if(animationNumber == 0) {
+                    constraintSetHalf.applyTo(mainContainer);
+                } else if(animationNumber == 1) {
                     constraintSetEnd.applyTo(mainContainer);
                 } else {
                     constraintSetStart.applyTo(mainContainer);
                 }
-                isToggled = !isToggled;
+
+                animationNumber++;
+
+                // Reset
+                if (animationNumber >2) {
+                    animationNumber = 0;
+                }
             }
         });
 
